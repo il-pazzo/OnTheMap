53940966bbdb05ef4c30b879be81552c1cad0939
Glenn Cole <gcole@mac.com>
2019-08-14 20:00:03 -0700

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Glenn Cole on 8/14/19.
+//  Copyright © 2019 Glenn Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
